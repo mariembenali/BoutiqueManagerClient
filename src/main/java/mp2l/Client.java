110f9f017109bfc4fdf6fdf0028df9a6df354a87
Mariem <mariembenali91@gmail.com>
2021-01-19 17:38:12 +0100

    Change 1: modifier devise

--- src/main/java/mp2l/Client.java
+++ src/main/java/mp2l/Client.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import mp2l.rmi.utils.LoadView;
 
-public class Main extends Application {
+public class Client extends Application {
 
 	@Override
 	public void start(Stage ecran) throws Exception {
--- src/main/java/mp2l/rmi/controller/MainViewController.java
+++ src/main/java/mp2l/rmi/controller/MainViewController.java
@@ -507,7 +507,7 @@ public class MainViewController {
 			}
 			actualiserCommListe(listeCommande);
 			int total = listeCommande.stream().filter(c -> c.getTotal() > 0).mapToInt(Detail::getTotal).sum();
-			txtTotalCommande.setText(total + " CFA");
+			txtTotalCommande.setText(total + "");
 
 		} else {
 			Utils.showMessage("", "", "Quantité non disponible en stock");
@@ -1285,7 +1285,7 @@ public class MainViewController {
 			foot.add(Chunk.TABBING);
 			foot.add(String.valueOf(c.getMontant()));
 
-			foot.add(" CFA\n\n");
+			foot.add("\n\n");
 			Font ft = new Font(Font.FontFamily.TIMES_ROMAN, 14f, Font.UNDERLINE);
 			ft.setStyle(Font.BOLDITALIC);
 			Paragraph sign = new Paragraph("Le Vendeur", ft);
@@ -1383,7 +1383,7 @@ public class MainViewController {
 			foot.add(Chunk.TABBING);
 			foot.add(String.valueOf(f.getCommande().getMontant()));
 
-			foot.add(" CFA\n\n");
+			foot.add("\n\n");
 
 			Font ft = new Font(Font.FontFamily.TIMES_ROMAN, 14f, Font.UNDERLINE);
 			ft.setStyle(Font.BOLDITALIC);
@@ -1400,7 +1400,7 @@ public class MainViewController {
 				foot1.setTabSettings(new TabSettings(350f));
 				foot1.add(Chunk.TABBING);
 				foot1.add(String.valueOf(f.getCommande().getAcompte()));
-				foot1.add(" CFA\n\n");
+				foot1.add("\n\n");
 				ct.addElement(foot1);
 			} else if (f.getClient().getType().getLibelle().equals("Entreprise") && f.getCommande().getEtat() == 1) {
 				Paragraph foot1 = new Paragraph("Montant Perçu :",
@@ -1408,7 +1408,7 @@ public class MainViewController {
 				foot1.setTabSettings(new TabSettings(350f));
 				foot1.add(Chunk.TABBING);
 				foot1.add(String.valueOf(f.getCommande().getMontant() - f.getCommande().getAcompte()));
-				foot1.add(" CFA\n\n");
+				foot1.add("\n\n");
 				ct.addElement(foot1);
 			}
 			ct.addElement(sign);
